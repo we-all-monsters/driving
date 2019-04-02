034b37d45a290adff0e344ffebf3118c745eea10
weallmonsters <weallmonsters@gmail.com>
2019-04-02 23:52:17 +0800

    add USA

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
 country = input('請問你是什麼地方的人：')
 age = input('你的年齡是：')
 age = int(age)
-if country == '香港' or 'HK':
+if country == '香港':
 	if age >= 18:
+		print('你可以考駕照')
+	else :
+		print('你還不可以考駕照')
+elif country == '美國':
+	if age >= 16:
 		print('你可以考駕照')
 	else :
 		print('你還不可以考駕照')
